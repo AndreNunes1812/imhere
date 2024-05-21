852f492ba9b7ea284eec4a8035a9cc6ebbffb8c2
Andre Nunes <andre.nunes020770@gmail.com>
2024-05-20 23:43:28 -0300

    criacao HOME

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,23 @@
+import {Text, TextInput, TouchableOpacity, View} from 'react-native';
+
+import {styles} from './styles';
+import React from 'react';
+
+export function Home() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.eventName}>Nome do Evento</Text>
+      <Text style={styles.eventDate}>Sexta, 4 de Novembro de 2022.</Text>
+
+      <TextInput
+        style={styles.input}
+        placeholder="Nome do Participante"
+        placeholderTextColor="$6B6B6B"
+      />
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>+</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
